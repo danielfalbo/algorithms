1a3f6aefe2d691453b3f8177bb146d779bcef110
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-04-23 15:14:29 +0200

    Solve Knapsack problem using dp

--- knapsack.py
+++ knapsack.py
@@ -1,3 +1,8 @@
+"""
+Knapsack Problem
+https://www.wikiwand.com/en/Knapsack_problem
+"""
+
 VALUE, SIZE = 0, 1
 
 
@@ -10,7 +15,38 @@ def knapsack(items, capacity):
     returns:
         s: subset of items that fits the knapsack with max value sum
     """
-    raise NotImplementedError
+    # mem[i][k] will be the best value we can get from
+    # the first i items for a knapsack of capacity k
+    mem = [[None for _ in range(capacity + 1)] for _ in range(len(items) + 1)]
+    for i in range(len(items) + 1):
+        mem[i][0] = 0
+    for k in range(capacity + 1):
+        mem[0][k] = 0
+
+    for i in range(1, len(items) + 1):
+        ith_item_val, ith_item_size = items[i - 1]
+        for smaller_capacity in range(1, capacity + 1):
+            if ith_item_size > smaller_capacity:
+                mem[i][smaller_capacity] = mem[i-1][smaller_capacity]
+            else:
+                mem[i][smaller_capacity] = max(
+                    ith_item_val + mem[i-1][smaller_capacity - ith_item_size],
+                    mem[i-1][smaller_capacity]
+                )
+
+    i, k = len(items), capacity
+    goal_value = mem[i][k]
+    s = []
+    while i > 0 and k > 0 and goal_value > 0:
+        if mem[i - 1][k] == goal_value:
+            i -= 1
+        else:
+            ith_item = ith_item_val, ith_item_size = items[i - 1]
+            s.append(ith_item)
+            i -= 1
+            k -= ith_item_size
+            goal_value -= ith_item_val
+    return s
 
 
 if __name__ == "__main__":
@@ -28,9 +64,8 @@ if __name__ == "__main__":
 
     menu = [wine, beer, pizza, burger, fries, coke, apple, donut]
 
-    try:
-        knapsack(menu, calorie_budget)
-    except NotImplementedError:
-        pass
+    order = knapsack(menu, calorie_budget)
+    assert sum(map(lambda item: item[SIZE], order)) == calorie_budget
+    assert sum(map(lambda item: item[VALUE], order)) == 359
 
     print("all tests successful")
