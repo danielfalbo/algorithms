05463704ade26f5708c435a956b8c834e688b004
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-06-29 16:54:15 +0200

    [max subarray prod] fix algo and add tests
    the algo wasn't correct

--- max_subarray_product.py
+++ max_subarray_product.py
@@ -15,21 +15,25 @@ def max_subarray_product(arr):
     args:
         arr: list of positive floats
     returns:
-        mx: largest subarray product of arr
+        p: largest subarray product of arr
     """
-    mx = arr[0]
-    acc = mx
-    for i in range(1, len(arr)):
-        if arr[i] > 1.0:
-            acc *= arr[i]
-            if acc > mx:
-                mx = acc
-        else:
-            acc = 1.0
+    n = len(arr)
+
+    # m[i] will be the max subarray product containing arr[i]
+    m = [None for _ in range(n)]
+    m[0] = arr[0]
+
+    mx = m[0]
+    for i in range(1, n):
+        m[i] = max(m[i - 1] * arr[i], arr[i])
+        if m[i] > mx:
+            mx = m[i]
     return mx
 
 
 if __name__ == "__main__":
     assert max_subarray_product([1.5, 4.0, 2.0, 0.1]) == 12.0
     assert max_subarray_product([2.0, 0.1, 2.0]) == 2.0
+    assert max_subarray_product([10.0, 0.5, 4.0]) == 20.0
+    assert max_subarray_product([0.2]) == 0.2
     print("all tests successful")
